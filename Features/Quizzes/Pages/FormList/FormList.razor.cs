b7378c4b7d4318d263afd92064bf0fdf3e965b7e
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-08-20 14:27:40 +0300

    feat(T2_FE): create a form list page

---
+++ Features/Quizzes/Pages/FormList/FormList.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Quiz.Features.Quizzes.Pages.FormList
+{
+    public partial class FormList
+    {
+        private void CreateNewForm()
+        {
+            Navigation.NavigateTo("/create-form");
+        }
+    }
+}
