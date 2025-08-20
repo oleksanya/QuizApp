2b3fdd413294637880af411729c505e392af82b6
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-08-20 14:50:18 +0300

    feat: create question card editor component

---
+++ Features/Quizzes/Components/QuestionEdit.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Quiz.Features.Quizzes.Components
+{
+    public partial class QuestionEdit : ComponentBase
+    {
+        [Parameter] public QuestionModel Question { get; set; } = default!;
+        [Parameter] public EventCallback OnRemove { get; set; }
+        
+        public void AddOption()
+        {
+            Question.Options.Add($"Option {Question.Options.Count + 1}");
+        }
+
+        public void AddOtherOption()
+        {
+            if (!Question.HasOtherOption)
+            {
+                Question.Options.Add("Other");
+                Question.HasOtherOption = true;
+            }
+        }
+
+        public void RemoveOption(int index)
+        {
+            if (index >= 0 && index < Question.Options.Count)
+            {
+                if (Question.Options[index] == "Other")
+                {
+                    Question.HasOtherOption = false;
+                }
+                Question.Options.RemoveAt(index);
+            }
+        }
+    }
+}
