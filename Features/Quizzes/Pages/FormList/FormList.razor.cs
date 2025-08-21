f86104e3fba766e8543b32b207404d97d7e0244c
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-08-21 11:25:39 +0300

    fix(FormCard, FormList): change button to "a" tag, remove injection from template, combine formCard-container css rules into one

--- Features/Quizzes/Pages/FormList/FormList.razor.cs
+++ Features/Quizzes/Pages/FormList/FormList.razor.cs
@@ -1,7 +1,10 @@
-﻿namespace Quiz.Features.Quizzes.Pages.FormList
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Quiz.Features.Quizzes.Pages.FormList
 {
     public partial class FormList
     {
+        [Inject] private NavigationManager Navigation { get; set; } = default!;
         private void CreateNewForm()
         {
             Navigation.NavigateTo("/create-form");
