c11ad7572f24922d4aad1c7f98a94af83d6ea004
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-08-21 11:40:23 +0300

    fix: remove onclick event, rename class from "btn" to "link"

--- Features/Quizzes/Pages/FormList/FormList.razor.cs
+++ Features/Quizzes/Pages/FormList/FormList.razor.cs
@@ -4,10 +4,6 @@ namespace Quiz.Features.Quizzes.Pages.FormList
 {
     public partial class FormList
     {
-        [Inject] private NavigationManager Navigation { get; set; } = default!;
-        private void CreateNewForm()
-        {
-            Navigation.NavigateTo("/create-form");
-        }
+
     }
 }
