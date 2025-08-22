e69c2614b865390072a83cfeda3ce82540e3cff8
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-08-22 15:11:06 +0300

    feat: update FormCard component with Form parameter

---
+++ Features/Quizzes/Components/FormCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Quiz.Features.Quizzes.Models;
+
+namespace Quiz.Features.Quizzes.Components
+{
+    public partial class FormCard
+    {
+        [Parameter] public CreateFormDto Form { get; set; } = default!;
+    }
+}
