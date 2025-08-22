1a050d3e0c5122a6f0bac14f7c888b9366cc8a52
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-08-22 14:29:54 +0300

    feat: update program.cs to use FormService and CustomHttpClient

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Quiz;
+using Quiz.Features.Quizzes.Services;
+using Quiz.Features.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:3002") });
+builder.Services.AddScoped<CustomHttpClient>();
+builder.Services.AddScoped<FormService>();
 
 await builder.Build().RunAsync();
