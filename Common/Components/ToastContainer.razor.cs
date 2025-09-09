7d8f195dfc03ed0f8829805e1ce7d08718a21b4a
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-09-09 14:53:48 +0300

    feat: add ToastContainer component for toast notifications

---
+++ Common/Components/ToastContainer.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Quiz.Common.Models;
+using Quiz.Common.Services;
+
+namespace Quiz.Common.Components
+{
+    public partial class ToastContainer
+    {
+        [Inject] private ToastService ToastService { get; set; } = default!;
+
+        private List<ToastModel> _toasts = new();
+
+        protected override void OnInitialized()
+        {
+            ToastService.OnToastsUpdated += UpdateToasts;
+            _toasts = ToastService.Toasts.ToList();
+        }
+
+        private void UpdateToasts()
+        {
+            _toasts = ToastService.Toasts.ToList();
+            InvokeAsync(StateHasChanged);
+        }
+
+        private Task RemoveToast(string id)
+        {
+            ToastService.RemoveToast(id);
+            return Task.CompletedTask;
+        }
+    }
+}
