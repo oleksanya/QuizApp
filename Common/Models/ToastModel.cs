109aa56d9170ad2f1d2056fd5e0c6b9369f32330
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-09-09 14:55:27 +0300

    feat: add ToastModel

---
+++ Common/Models/ToastModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz.Common.Models
+{
+    public enum ToastType { Success, Error, Warning, Info }
+
+    public class ToastModel
+    {
+        public string Id { get; init; } = Guid.NewGuid().ToString("N");
+        public string? Title { get; init; }
+        public required string Message { get; init; }
+        public ToastType Type { get; init; } = ToastType.Info;
+        public int DurationMs { get; init; } = 5000;
+    }
+}
