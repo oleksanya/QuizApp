3adaa7c4ad3a0f31c8b9e0d4f492a6475d553d54
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-09-09 14:39:00 +0300

    feat: add Toast component with icons and progress bar

---
+++ Common/Components/Toast.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Quiz.Common.Models;
+using Quiz.Common.Services;
+
+namespace Quiz.Common.Components
+{
+    public partial class Toast
+    {
+        [Parameter] public ToastModel ToastModel { get; set; } = default!;
+        [Parameter] public EventCallback<string> OnRemove { get; set; }
+
+        [Inject] private ToastService ToastService { get; set; } = default!;
+
+        private bool IsVisible { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(50);
+            IsVisible = true;
+        }
+
+        private async Task OnCloseClick()
+        {
+            await OnRemove.InvokeAsync(ToastModel.Id);
+        }
+    }
+}
