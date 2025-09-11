53c4366da768cb99f0c1519657e4ee198ba4222b
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-09-11 10:08:23 +0300

    fix: remove unused function

--- wwwroot/js/theme.js
+++ wwwroot/js/theme.js
@@ -6,20 +6,5 @@ window.themeUtils = {
     
     getSystemTheme: function() {
         return window.matchMedia('(prefers-color-scheme: dark)').matches;
-    },
-    
-    // Listen for system theme changes
-    watchSystemTheme: function(dotNetRef) {
-        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-        
-        function handleThemeChange(e) {
-            dotNetRef.invokeMethodAsync('OnSystemThemeChanged', e.matches);
-        }
-        
-        mediaQuery.addEventListener('change', handleThemeChange);
-        
-        return function() {
-            mediaQuery.removeEventListener('change', handleThemeChange);
-        };
     }
 };
